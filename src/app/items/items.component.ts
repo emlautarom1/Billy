38747930976bb11aa9bd8cfdb0b391f9ea00385e
Lautaro Emanuel <emlautarom1@gmail.com>
2022-03-10 09:27:25 -0300

    Add using FabButton, use prop as source

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Item } from '../model/item';
 
 @Component({
   selector: 'app-items',
@@ -7,8 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {}
+  items: Item[] = [
+    { name: "Pizza", price: 850 },
+    { name: "Beer", price: 300 },
+  ];
+
+  ngOnInit() { }
+
+  async addItem() {
+    const alert = await this.alertController.create({
+      header: "Add Item",
+      inputs: [
+        {
+          name: "name",
+          type: "text",
+          placeholder: "Item"
+        },
+        {
+          name: "price",
+          type: "number",
+          placeholder: "$",
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+        },
+        {
+          text: "Add",
+          role: "submit"
+        }
+      ]
+    });
+
+    await alert.present();
+
+    const { data, role } = await alert.onDidDismiss();
+    if (role == "submit") {
+      const item = data.values;
+      this.items = [...this.items, item];
+    }
+  }
 
 }
--- src/app/participants/participants.component.ts
+++ src/app/participants/participants.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Participant } from '../model/participant';
 
 @Component({
   selector: 'app-participants',
@@ -7,8 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParticipantsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {}
+  participants: Participant[] = [
+    { name: "Ana" },
+    { name: "Bob" }
+  ];
+
+  ngOnInit() { }
+
+  async addParticipant() {
+    const alert = await this.alertController.create({
+      header: "Add Participant",
+      inputs: [
+        {
+          name: "name",
+          type: "text",
+          placeholder: "Name"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+        },
+        {
+          text: "Add",
+          role: "submit"
+        }
+      ]
+    });
+
+    await alert.present();
+
+    const { data, role } = await alert.onDidDismiss();
+    if (role == "submit") {
+      const participant = data.values;
+      this.participants = [...this.participants, participant];
+    }
+  }
 
 }
