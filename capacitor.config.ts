54f00d0a3b56af12cf12bf2d02da1d84170b6c04
Lautaro Emanuel <emlautarom1@gmail.com>
2022-03-12 01:48:37 -0300

    Rollback deployment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.github.emlautarom1',
   appName: 'billy',
-  webDir: 'docs',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
